93f4ff187ecad5d018b2a9770009520dba3c6c59
Mamadou WONE <mamadu.wone@gmail.com>
2022-01-17 18:57:19 +0000

    create dynamic array

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("amir")
+import sys
+
+# def sum1(n):
+#     return (n * (n + 1)) / 2
+
+
+n = 10
+data = []
+
+for i in range(n):
+    a = len(data)
+    b = sys.getsizeof(data)
+    print('Length: {0:3d}; Size in bytes: {1:4d} '. format(a, b))
+    data.append(n)
